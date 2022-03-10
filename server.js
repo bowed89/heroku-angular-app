8166072938e6e7985da731597e07c2d37bc4eea3
Jesús López Averanga <jesus.lopez.averanga@gmail.com>
2022-03-10 13:00:33 -0400

    tercer commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sakai-ng'));
+app.use(express.static('./dist/sakai'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/sakai-ng/'}),
+    res.sendFile('index.html', {root: 'dist/sakai/'}),
 );
 
 // Start the app by listening on the default Heroku port
