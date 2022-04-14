77e11df62ff2fdb77383211ea27261a9db870c9d
bowed89 <jesus.lopez.averanga@gmail.com>
2022-04-13 22:24:02 -0400

    modificacion de config.ts

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
-export const URL_SERVICIOS = 'http://localhost:3000/api/v1';
-//export const URL_SERVICIOS = 'https://app-sistemadb-nodejs.herokuapp.com/api/v1';
+//export const URL_SERVICIOS = 'http://localhost:3000/api/v1';
+export const URL_SERVICIOS = 'https://app-sistemadb-nodejs.herokuapp.com/api/v1';
